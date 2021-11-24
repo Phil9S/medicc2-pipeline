875d92702da974d5aa8be1e8f3466af6df96814a
Phil9S <philip.smith@cruk.cam.ac.uk>
2021-11-24 19:21:58 +0000

    qdnaseq method implemented and functional

--- run_medicc2.sh
+++ run_medicc2.sh
@@ -3,6 +3,11 @@
 # Default vars
 ECHO=$(echo [$0])
 TYPE="qdnaseq"
+NOWGD=""
+TCN=""
+CORES=8
+DEBUG=FALSE
+ARGS=""
 
 # Stop/error function
 stopFunction(){
@@ -17,18 +22,28 @@ trap 'stopFunction' SIGINT SIGTERM
 helpFunction(){
     echo -e "${ECHO}[`date "+%H:%M:%S"`][INIT] Wrapper script for multi-sample and variable input types for medicc2"
     echo -e "Options:"
+    echo -e "\tArguments:"
+    echo -e "\t\t-t\tinput type (either qdnaseq | segment | medicc)"
+    echo -e "\t\t-i\tFull path to input file matching input type specified by -t"
+    echo -e "\t\t-o\tFull path output directory for Medicc2 results"
+    echo -e "\t\t-m\tFull path to meta file detailing patient-sample relationship, where -t is not medicc"
+    echo -e "\t\t-a\tAdditional arguments passed to medicc2 (must be quoted)"
+    echo -e "\tFlags:"
+    echo -e "\t\t-w\tSet medicc2 to not use whole genome duplication in tree calculations"
+    echo -e "\t\t-n\tSet midicc2 to use total copy number (default column name 'cn_a')"
+    echo -e "\t\t-wn\tSet both options above"
+    echo -e ""
 }
 
 makeDirs(){
-    if ! [ -d "${OUPUT}input_files/" ]; then
+    if ! [ -d "${OUTPUT}input_files/" ]; then
         mkdir ${OUTPUT}input_files/
     fi
 
-    if ! [ -d "${OUPUT}medicc2_output/" ]; then
+    if ! [ -d "${OUTPUT}medicc2_output/" ]; then
         mkdir ${OUTPUT}medicc2_output/
     fi
 }
-
 
 set -e
 
@@ -36,7 +51,7 @@ if [[ $# -eq 0 ]]; then
 	helpFunction; exit 0
 fi
 
-while getopts "hwci:o:t:" opt; do
+while getopts "hwdni:o:t:c:m:a:" opt; do
   case $opt in
     h ) helpFunction ; exit 0
     ;;
@@ -50,14 +65,40 @@ while getopts "hwci:o:t:" opt; do
             stopFunction
         }
     ;;
-    : ) echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Invalid option: $OPTARG requires an argument" ; stopFunction
+    m ) META="$OPTARG"
+    ;;
+    c ) CORES="$OPTARG"
+    ;;
+    w ) NOWGD=" --no-wgd"
+    ;;
+    n ) TCN=" --total-copy-number -a cn_a"
+    ;;
+    a ) ARGS="$OPTARG"
+    ;;
+    d ) DEBUG=TRUE
+    ;;
+    :  ) echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Invalid option: $OPTARG requires an argument" ; stopFunction
     ;;
     #\? ) echo "Invalid option: $OPTARG" ; stopFunction
     #;;
-    #*  ) echo "Unimplemented option: $OPTARG" ; stopFunction
+    #*  ) echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Unimplemented option: $OPTARG" ; stopFunction
     #;;
   esac
 done
+
+## VAR PRINT CHECK
+if [ "$DEBUG" == TRUE ]; then
+    echo -e "\${INPUT}=${INPUT}"
+    echo -e "\${OUPUT}=${OUTPUT}"
+    echo -e "\${META}=${META}"
+    echo -e "\${TYPE}=${TYPE}"
+    echo -e "\${NOWGD}=${NOWGD}"
+    echo -e "\${TCN}=${TCN}"
+    echo -e "\${DEBUG}=${DEBUG}"
+    echo -e "\${CORES}=${CORES}"
+    echo -e "\${ARGS}=${ARGS}"
+    exit 0
+fi
 
 ## Check input file
 if ! [ -f "${INPUT}" ]; then
@@ -66,6 +107,17 @@ if ! [ -f "${INPUT}" ]; then
 elif ! [ -r "${INPUT}" ]; then
     echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Input file not readable"
     stopFunction
+fi
+
+## Check META file
+if [ "$TYPE" != "medicc" ]; then
+    if ! [ -f "${META}" ]; then
+        echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Metadata file does not exist"
+        stopFunction
+    elif ! [ -r "${META}" ]; then
+        echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Metadata file not readable"
+        stopFunction
+    fi
 fi
 
 ## Check output directory exists and writable 
@@ -83,16 +135,39 @@ if ! [ -x "$(command -v medicc2)" ]; then
     stopFunction
 fi
 
+echo -e "${ECHO}[`date "+%H:%M:%S"`][INIT] Making working directories"
+makeDirs
+
 if [ "$TYPE" == "qdnaseq" ]; then
     ## Check programmes available
+    echo -e "${ECHO}[`date "+%H:%M:%S"`][INIT] Input type set to QDNASEQ object"
     if ! [ -x "$(command -v Rscript)" ]; then
         echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Rscript unavailable - check conda environment"
         stopFunction
     fi
-    echo -e "QDNASEQ"
+    echo -e "${ECHO}[`date "+%H:%M:%S"`][MAIN] Extracting medicc2 input format"
     ## Build output directory structure
-    makeDirs
-    #Rscript scripts/qdnaseq_to_medicc_format_script.R ${INPUT} ${OUPUT}input_files/
+    Rscript scripts/qdnaseq_to_medicc_format_script.R ${INPUT} ${OUTPUT}input_files/ ${META}
+elif [ "$TYPE" == "segment" ]; then
+    echo -e "${ECHO}[`date "+%H:%M:%S"`][INIT] Input type set to segment table"
+    if ! [ -x "$(command -v Rscript)" ]; then
+        echo -e "${ECHO}[`date "+%H:%M:%S"`][ERROR] Rscript unavailable - check conda environment"
+        stopFunction
+    fi
+    echo -e "${ECHO}[`date "+%H:%M:%S"`][MAIN] Extracting medicc2 input format"
+    ## Build output directory structure
+    #Rscript scripts/segment_to_medicc_format_script.R ${INPUT} ${OUTPUT}input_files/ ${META}
+elif [ "$TYPE" == "medicc" ]; then
+    echo -e "${ECHO}[`date "+%H:%M:%S"`][INIT] Input type set to medicc"
+    #cp ${INPUT} ${OUTPUT}input_files/
 fi
 
-echo "END"
+echo -e "${ECHO}[`date "+%H:%M:%S"`][MAIN] Running medicc2"
+find ${OUTPUT}input_files/ -name "*" -type f | xargs -P ${CORES}  -n 1 -I {} medicc2 {} ${OUTPUT}medicc2_output/${NOWGD}${TCN} ${ARGS}
+
+echo -e "${ECHO}[`date "+%H:%M:%S"`][MAIN] Generating summary information"
+## Combine trees
+cat ${OUTPUT}medicc2_output/*final_tree.new > ${OUTPUT}medicc2_output/all.trees.new
+
+echo -e "${ECHO}[`date "+%H:%M:%S"`][MAIN] Pipeline complete"
+## END
--- scripts/qdnaseq_to_medicc_format_script.R
+++ scripts/qdnaseq_to_medicc_format_script.R
@@ -2,6 +2,8 @@
 # Function written by Michael Schneider
 # Modified and debugged by Philip Smith
 
+args = commandArgs(trailingOnly=TRUE)
+
 # Required libraries
 require(Biobase, quietly = TRUE, warn.conflicts = FALSE)
 require(QDNAseqmod, quietly = TRUE, warn.conflicts = FALSE)
@@ -15,7 +17,7 @@ getMediccTable <- function(object, value="copynumber"){
     valid = object@featureData@data$use
     info = rownames(object)
     #seqName = unlist(lapply(strsplit(info, ":"),[,1]))
-    seqName =unlist(lapply(strsplit(info, ":"),FUN = function(x) x[1]))
+    seqName = unlist(lapply(strsplit(info, ":"),FUN = function(x) x[1]))
     chrOrder = base::rle(seqName)[["values"]]
     chromosome <- factor(seqName, levels=chrOrder)
     ids = paste0("chr",chromosome,":", apply(CN, 1, function(x){
@@ -30,21 +32,27 @@ getMediccTable <- function(object, value="copynumber"){
                                                                          width=runLength(rl), sample=sample, copynumber = cn_values))
         df = data.frame(gr) %>% dplyr::rename("chrom"="seqnames", "sample_id"="sample", "cn_a"=copynumber) %>% dplyr::select(-strand, -width) %>%
             dplyr::relocate(sample_id, chrom, start, end, cn_a)
+        # replace index with actual position
+        df$start <- fData(object)$start[df$start]
+        df$end <- fData(object)$start[df$end]
         return(df)
     }
     dfs = dplyr::bind_rows(lapply(1:ncol(object), export_single_sample))
     return(dfs)
 }
 
+# Load sample CN
+print(args)
+abs_data <- readRDS(args[1])
+
 # Set output folder
-outfolder <- "input_files"
+outfolder <- args[2]
 
 # Load sample meta.data
-meta.data <- read.table("britroc_30kb_signature_data_meta.tsv",header = TRUE,sep = "\t")
+meta.data <- read.table(args[3],header = TRUE,sep = "\t")
 
-# Load sample CN
-abs_data <- readRDS("britroc_30kb_ds_absCopyNumber.rds")
-abs_data <- abs_data[,which(colnames(abs_data) %in% meta.data$SAMPLE_ID[meta.data$use == "TRUE"])]
+# Filter data and meta to match
+abs_data <- abs_data[,which(colnames(abs_data) %in% meta.data$SAMPLE_ID)]
 
 # Split sample lists by patient
 sample_by_pat_list <- split(meta.data$SAMPLE_ID,f=as.factor(meta.data$PATIENT_ID))
@@ -60,9 +68,10 @@ medicc.table <- lapply(names(sample_by_pat_list),FUN = function(x){
     if(length(y) > 1){
         object <- abs_data[,colnames(abs_data) %in% y]
         table <- getMediccTable(object = object,value = "segmented")
-	table$cn_a[table$cn_a < 0] <- 0
-        write.table(x = table,file = paste0(outfolder,"/",patient_name),quote = F,sep = "\t",row.names = F,col.names = T)
+	      table$cn_a[table$cn_a < 0] <- 0
+        write.table(x = table,file = paste0(outfolder,patient_name),quote = F,sep = "\t",row.names = F,col.names = T)
     }
+    return(table)
 })
 
 ## END
